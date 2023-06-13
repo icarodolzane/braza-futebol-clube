e8bfb003426f7429f4e72a37cae70de0a6f5db9d
Icaro <icarodolzane@live.com>
2023-06-13 19:34:11 -0400

    feat: Criado model sequelize e migration de matches

---
+++ app/backend/src/database/models/SequelizeMatch.ts
@@ -0,0 +1,72 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+import Teams from './SequelizeTeam';
+
+class SequelizeMatches extends Model<InferAttributes<SequelizeMatches>,
+InferCreationAttributes<SequelizeMatches>> {
+  declare id: CreationOptional<number>;
+
+  declare homeTeamId: number;
+
+  declare homeTeamGoals: number;
+
+  declare awayTeamId: number;
+
+  declare awayTeamGoals: number;
+
+  declare inProgress: boolean;
+}
+
+SequelizeMatches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Teams,
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Teams,
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+SequelizeMatches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+SequelizeMatches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default SequelizeMatches;
